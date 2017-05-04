18f309173be79ae7e71364295ed63776d503ffc8
Forkmeplz <rasse_mus@live.se>
2017-05-04 13:17:38 +0200

    Add files via upload

---
+++ PauseGame.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PauseGame : MonoBehaviour {
+    public Transform Canvas;
+    public Transform player;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape)) 
+        {
+            if (Canvas.gameObject.activeInHierarchy == false)
+            {
+                Canvas.gameObject.SetActive(true);
+                Time.timeScale = 0;
+                Cursor.visible = true;
+                player.GetComponent<controller>().enabled = false;
+            }
+            else 
+            {
+                Canvas.gameObject.SetActive(false);
+                Time.timeScale = 1;
+                Cursor.visible = false;
+                player.GetComponent<controller>().enabled = true;
+            }
+        }
+	}
+}
---
+++ bullet.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bullet : MonoBehaviour {
+
+    public GameObject bullet_prefab;
+    public float bulletimpulse;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("Fire1"))
+        { 
+            Camera cam = Camera.main;
+            GameObject thebullet = (GameObject)Instantiate(bullet_prefab, cam.transform.position, cam.transform.rotation);
+            thebullet.GetComponent<Rigidbody>().AddForce(cam.transform.forward * bulletimpulse, ForceMode.Impulse);
+            
+        }
+	}
+}
